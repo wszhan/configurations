90ca2e06935a4b51a91d90916b01ee6d2a324555
wszhan <19308564+wszhan@users.noreply.github.com>
2024-02-06 18:25:00 -0500

    Create init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,18 @@
+-- Indentation options
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+-- Features
+vim.opt.compatible = false
+vim.opt.filetype = "on"
+vim.opt.syntax = "on"
+
+-- When opening a new line, keep the same indent as the line you're currently on.
+vim.opt.autoindent = true
+
+-- Display line numbers on the left
+vim.opt.number = true
+
+-- Hybrid line numbers
+vim.opt.relativenumber = true
